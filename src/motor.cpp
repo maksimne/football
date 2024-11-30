9cb0cd0632433d8fa16ff777a741d52bedbf892e
maksimne <godonukm@gmail.com>
2024-11-30 15:46:24 +0300

    go

---
+++ src/motor.cpp
@@ -0,0 +1,41 @@
+// #include <Cdrv8833.h>
+// #define M1 2
+// #define M1PWM 15
+// #define M2 4
+// #define M2PWM 16
+// #define M3 19
+// #define M3PWM 18
+// #define M4 5
+// #define M4PWM 17
+// #define KICKER 23
+// #define DRIBBLER 33
+
+
+// Cdrv8833 m1;
+// Cdrv8833 m2;
+// Cdrv8833 m3;
+// Cdrv8833 m4;
+
+
+// void init_motor()
+// {
+//   m1.init(M1, M1PWM, 6, 1);
+//   m2.init(M2, M2PWM, 5, 1);
+//   m3.init(M3, M3PWM, 2, 1);
+//   m4.init(M4, M4PWM, 3, 1);
+
+//   m1.setDecayMode(drv8833DecaySlow);
+//   m2.setDecayMode(drv8833DecaySlow);
+//   m3.setDecayMode(drv8833DecaySlow);
+//   m4.setDecayMode(drv8833DecaySlow);
+// }
+
+// void move()
+// {
+//     m1.move(50);
+//     m2.move(50);
+//     m3.move(50);
+//     m4.move(50);
+// }
+
+
---
+++ src/motor.h
@@ -0,0 +1,18 @@
+// #pragma once
+// #include <Arduino.h>
+
+// class Motor
+// {
+// private:
+//     /* data */
+// public:
+
+// };
+
+
+// Motor :: Motor(int speed)
+// {
+//     _speed = speed;
+
+// }
+
